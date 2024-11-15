9bfb43f9b1fca0425377300b2546404090ee2536
baehl <baehl44@yahoo.fr>
2024-11-15 10:39:35 +0100

    creaction de la première fonction getLatinCharacterList

---
+++ .github/script.js
@@ -0,0 +1,6 @@
+//fonction va prendre en paramètre du texte et retourner un tableau contenant chaque caractère
+function getLatinCharacterList(Mytexte){
+    let myTexteArray= Mytexte.split('')
+    console.log(myTexteArray)
+}
+getLatinCharacterList("Hello,world");
